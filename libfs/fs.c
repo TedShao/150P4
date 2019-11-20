1ddb2ad43e060a41defeb3a5b9a611b6cfa4a607
TedShao <tedshao213@gmail.com>
2019-11-20 15:15:21 -0800

    Update fs.c

--- libfs/fs.c
+++ libfs/fs.c
@@ -8,20 +8,92 @@
 #include "fs.h"
 
 /* TODO: Phase 1 */
+struct superblock {
+	int8_t signature;
+	int16_t block_amount;
+	int16_t root_dir_index;
+	int16_t data_block_index;
+	int16_t data_block_amount;
+	int8_t fat_block;
+	int8_t padding[4079];
+};
 
+struct fat {
+	int16_t entry;
+};
+
+struct root_dir {
+	int8_t filename[16];
+	int32_t filesize;
+	int16_t first_index;
+	int8_t padding[10];
+};
+
+struct fd {
+	root_dir_t *root_dir;
+	size_t offset;
+};
+
+superblock_t superBlk;
+fd_t fd_tmp;
+/**
+ * fs_mount - Mount a file system
+ * @diskname: Name of the virtual disk file
+ *
+ * Open the virtual disk file @diskname and mount the file system that it
+ * contains. A file system needs to be mounted before files can be read from it
+ * with fs_read() or written to it with fs_write().
+ *
+ * Return: -1 if virtual disk file @diskname cannot be opened, or if no valid
+ * file system can be located. 0 otherwise.
+ */
 int fs_mount(const char *diskname)
 {
-	/* TODO: Phase 1 */
+	if (block_disk_open(diskname) == -1) {
+		return -1;
+	}
+	if (block_read(0, superBlk) == -1) {
+		return -1;
+	}
+	size_t i;
+	char
+
+
+
+
+	for (i = 0; i < 32; i++) {
+		fd_tmp->root_dir = NULL;
+		fd_tmp->offset = 0;
+	}
+	return 0;
+
 }
 
 int fs_umount(void)
 {
-	/* TODO: Phase 1 */
+	if (block_write(0, superBlk) == -1) {
+		return -1;
+	}
 }
 
 int fs_info(void)
 {
-	/* TODO: Phase 1 */
+    size_t i;
+    int free_blocks_count = 0;
+
+    for (i = 1; i < superBlk.data_block_amount; i++) {
+            if (fat.entry[i] == 0)
+                    free_blocks_count++;
+    }
+    printf("FS Info:\n");
+    printf("total_blk_count=%u\n", superBlk.data_block_amount);
+    printf("fat_blk_count=%u\n", superBlk.fat_block);
+    printf("rdir_blk=%u\n", superBlk.root_dir_index);
+    printf("data_blk=%u\n", superBlk.data_block_index);
+    printf("data_blk_count=%u\n", superBlk.data_block_amount);
+    printf("fat_free_ratio=%u/%u\n", free_blocks_count, superBlk.data_block_amount);
+    printf("rdir_free_ratio=%u/%u\n", free_rdir_count, 128);
+
 }
 
 int fs_create(const char *filename)

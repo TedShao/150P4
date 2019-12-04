e81115dee7da8f9c1ad83bdecf97482c0a4b680d
TedShao <tedshao213@gmail.com>
2019-12-04 13:28:25 -0800

    Update fs.c

--- libfs/fs.c
+++ libfs/fs.c
@@ -5,7 +5,7 @@
 #include <string.h>
 
 #include "disk.h"
-#include "fs.h" 
+#include "fs.h"
 
 #define SIG "ECS150FS"
 #define FAT_EOC 0xffff
@@ -38,7 +38,7 @@ typedef struct __attribute__((__packed__)) Fd {
     size_t offset;
 } *Fd_t;
 
-Fd_t File_descriptors = NULL;
+Fd_t fds = NULL;
 
 int fs_mount(const char *diskname)
 {
@@ -60,15 +60,6 @@ int fs_mount(const char *diskname)
         return -1;
     }
     if (block_disk_count() != superblock->total_blks) {
-        return -1;
-    }
-    if (superblock->root_dir_idx != superblock->total_fat_blks + 1) {
-        return -1;
-    }
-    if (superblock->data_blk_idx != superblock->root_dir_idx + 1) {
-        return -1;
-    }
-    if (superblock->total_data_blks == 0) {
         return -1;
     }
 
@@ -94,13 +85,13 @@ int fs_mount(const char *diskname)
         return -1;
     }
 
-    /* set default fd opened files */
-    File_descriptors = (Fd_t)malloc(FS_OPEN_MAX_COUNT * sizeof(struct Fd));
-    if (File_descriptors == NULL) {
+    /* Phase 3: set default fd opened files */
+    fds = (Fd_t)malloc(FS_OPEN_MAX_COUNT * sizeof(struct Fd));
+    if (fds == NULL) {
         return -1;
     }
     for (int i = 0; i < 32; i++) {
-        File_descriptors[i].open_file = NULL;
+        fds[i].open_file = NULL;
     }
 
     return 0;
@@ -115,23 +106,20 @@ int fs_umount(void)
 
     /* if there exists any opened file */
     for (int i = 0; i < 32; i++) {
-        if (File_descriptors[i].open_file != NULL) {
+        if (fds[i].open_file != NULL) {
             return -1;
         }
     }
-    /* write & check superblock */
+    /* write backs */
     if (block_write(0, superblock) == -1) {
         return -1;
     }
-
-    /* write & check fat blocks */
     for (int i = 0; i < superblock->total_fat_blks; i++) {
         if (block_write(i+1, fat_array + (i * BLOCK_SIZE) / 2) == -1) {
             return -1;
         }
     }
 
-    /* write & check root directory */
     if (block_write(superblock->root_dir_idx, root_dir) == -1) {
         return -1;
     }
@@ -151,8 +139,8 @@ int fs_umount(void)
     if (fat_array != NULL) {
         free(fat_array);
     }
-    if (File_descriptors != NULL) {
-        free(File_descriptors);
+    if (fds != NULL) {
+        free(fds);
     }
     return 0;
 }
@@ -165,14 +153,14 @@ int fs_info(void)
     }
 
     /* count the number of free data blocks */
-    int free_blocks_count = 0;
+    int free_blks = 0;
     for (int i = 0; i < superblock->total_data_blks; i++) {
         if (fat_array[i] == 0) {
-            free_blocks_count++;
+            free_blks++;
         }
     }
 
-    /* count the number of free root directories*/
+    /* count the number of free root directories */
     int free_rdir_count = 0;
     for (int i = 0; i < FS_FILE_MAX_COUNT; ++i) {
         if (root_dir[i].filename[0] == '\0') {
@@ -187,7 +175,7 @@ int fs_info(void)
     printf("rdir_blk=%u\n", superblock->root_dir_idx);
     printf("data_blk=%u\n", superblock->data_blk_idx);
     printf("data_blk_count=%u\n", superblock->total_data_blks);
-    printf("fat_free_ratio=%u/%u\n", free_blocks_count, superblock->total_data_blks);
+    printf("fat_free_ratio=%u/%u\n", free_blks, superblock->total_data_blks);
     printf("rdir_free_ratio=%u/%u\n", free_rdir_count, 128);
 
     return 0;
@@ -196,6 +184,9 @@ int fs_info(void)
 int fs_create(const char *filename)
 {
     /* check valid filename */
+    if (filename == NULL) {
+        return -1;
+    }
     if (strlen(filename) == 0 || strlen(filename) > FS_FILENAME_LEN) {
         return -1;
     }
@@ -226,28 +217,32 @@ int fs_create(const char *filename)
 
 int fs_delete(const char *filename)
 {
+    if (filename == NULL) {
+        return -1;
+    }
     if (strlen(filename) == 0 || strlen(filename) > FS_FILENAME_LEN) {
         return -1;
     }
 
     for (int i = 0; i < FS_OPEN_MAX_COUNT; i++) {
-        if (File_descriptors->open_file != NULL && strcmp(filename, (char*)File_descriptors->open_file->filename) == 0) {
+        if (fds->open_file != NULL
+          && strcmp(filename, (char*)fds->open_file->filename) == 0) {
             return -1;
         }
     }
 
-    int targetIndex = -1;
+    int idx = -1;
     for (int i = 0; i < FS_FILE_MAX_COUNT; i++) {
-        if (strcmp(filename, (char*)root_dir[i].filename) == 0 && targetIndex == -1) {
-            targetIndex = i;
+        if (strcmp(filename, (char*)root_dir[i].filename) == 0 && idx == -1) {
+            idx = i;
         }
     }
-    if (targetIndex == -1) {
+    if (idx == -1) {
         return -1;
     }
 
     /* free file's conetent in FAT */
-    int delete_blk_idx = root_dir[targetIndex].first_blk_index;
+    int delete_blk_idx = root_dir[idx].first_blk_index;
     while (delete_blk_idx != FAT_EOC) {
         uint16_t temp = fat_array[delete_blk_idx];
         fat_array[delete_blk_idx] = 0;
@@ -255,13 +250,12 @@ int fs_delete(const char *filename)
     }
 
     /* reset related content in root directory */
-    memset(&(root_dir[targetIndex]), 0, BLOCK_SIZE/FS_FILE_MAX_COUNT);
-    strcpy((char*)root_dir[targetIndex].filename, "\0");
-    root_dir[targetIndex].filesize = 0;
-    root_dir[targetIndex].first_blk_index = 0;
+    memset(&(root_dir[idx]), 0, BLOCK_SIZE/FS_FILE_MAX_COUNT);
+    strcpy((char*)root_dir[idx].filename, "\0");
+    root_dir[idx].filesize = 0;
+    root_dir[idx].first_blk_index = 0;
     return 0;
 }
-
 
 int fs_ls(void)
 {
@@ -284,26 +278,29 @@ int fs_ls(void)
 int fs_open(const char *filename)
 {
     /* check if filename is valid */
+    if (filename == NULL) {
+        return -1;
+    }
     if (strlen(filename) == 0 || strlen(filename) > FS_FILENAME_LEN) {
         return -1;
     }
 
     /* find file location */
-    int file_location = -1;
+    int f_loc = -1;
     for (int i = 0; i < FS_FILE_MAX_COUNT; i++) {
-        if (strcmp(filename, (char*)root_dir[i].filename) == 0 && file_location == -1) {
-            file_location = i;
+        if (strcmp(filename, (char*)root_dir[i].filename) == 0 && f_loc == -1) {
+            f_loc = i;
         }
     }
     // file named filename not found
-    if (file_location == -1) {
+    if (f_loc == -1) {
         return -1;
     }
 
     /* find free file descriptor location */
     int fd_idx = -1;
     for (int i = 0; i < FS_OPEN_MAX_COUNT; i++) {
-        if (fd_idx == -1 && File_descriptors[i].open_file == NULL) {
+        if (fd_idx == -1 && fds[i].open_file == NULL) {
             fd_idx = i;
         }
     }
@@ -312,8 +309,8 @@ int fs_open(const char *filename)
         return -1;
     }
 
-    File_descriptors[fd_idx].open_file = &(root_dir[file_location]);
-    File_descriptors[fd_idx].offset = 0;
+    fds[fd_idx].open_file = &(root_dir[f_loc]);
+    fds[fd_idx].offset = 0;
 
     return fd_idx;
 }
@@ -324,12 +321,12 @@ int fs_close(int fd)
         return -1;
     }
 
-    if (File_descriptors[fd].open_file == NULL) {
+    if (fds[fd].open_file == NULL) {
         return -1;
     }
 
-    File_descriptors[fd].open_file = NULL;
-    File_descriptors[fd].offset = 0;
+    fds[fd].open_file = NULL;
+    fds[fd].offset = 0;
 
     return 0;
 }
@@ -340,11 +337,11 @@ int fs_stat(int fd)
         return -1;
     }
 
-    if (File_descriptors[fd].open_file == NULL) {
+    if (fds[fd].open_file == NULL) {
         return -1;
     }
 
-    return File_descriptors[fd].open_file->filesize;
+    return fds[fd].open_file->filesize;
 }
 
 int fs_lseek(int fd, size_t offset)
@@ -353,197 +350,167 @@ int fs_lseek(int fd, size_t offset)
         return -1;
     }
 
-    if (File_descriptors[fd].open_file == NULL) {
+    if (fds[fd].open_file == NULL) {
         return -1;
     }
 
-    if (offset > File_descriptors[fd].open_file->filesize) {
+    if (offset > fds[fd].open_file->filesize) {
         return -1;
     }
 
-    File_descriptors[fd].offset = offset;
+    fds[fd].offset = offset;
 
     return 0;
 }
 
-/**
- * fs_write - Write to a file
- * @fd: File descriptor
- * @buf: Data buffer to write in the file
- * @count: Number of bytes of data to be written
- *
- * Attempt to write @count bytes of data from buffer pointer by @buf into the
- * file referenced by file descriptor @fd. It is assumed that @buf holds at
- * least @count bytes.
- *
- * When the function attempts to write past the end of the file, the file is
- * automatically extended to hold the additional bytes. If the underlying disk
- * runs out of space while performing a write operation, fs_write() should write
- * as many bytes as possible. The number of written bytes can therefore be
- * smaller than @count (it can even be 0 if there is no more space on disk).
- *
- * Return: -1 if file descriptor @fd is invalid (out of bounds or not currently
- * open). Otherwise return the number of bytes actually written.
- */
-
 int fs_write(int fd, void *buf, size_t count)
 {
+    /* handle error */
     if (fd < 0 || fd >= FS_OPEN_MAX_COUNT) {
         return -1;
     }
-    if (File_descriptors[fd].open_file == NULL) {
+    if (fds[fd].open_file == NULL) {
         return -1;
     }
 
-    if (count == 0) {
-        return 0;
-    }
-    //if no space
-    if (File_descriptors[fd].open_file->first_blk_index == FAT_EOC) {
-        //extend
-
-        // int free_blocks_count = 0;
-        // for (int i = 0; i < superblock->total_data_blks; i++) {
-        //     if (fat_array[i] == 0) {
-        //         free_blocks_count++;
-        //     }
-        // }
-
-        size_t new_free_block;
-        for (int i = 0; i < superblock->total_data_blks;i++) {
-            if (i == superblock->total_data_blks) {
-                new_free_block = 0;
-            }
-            if (fat_array[i] == 0) {
-              new_free_block = i;
-              break;
+    /* allocate space if needed */
+    if (fds[fd].offset + count > fds[fd].open_file->filesize) {
+        uint16_t cur;
+        if (fds[fd].open_file->first_blk_index == FAT_EOC) {
+            // nxt = first_free_block();
+            for (int j = 1; j < superblock->total_data_blks; j++) {
+                if (fat_array[j] == 0) {
+                    fds[fd].open_file->first_blk_index = j;
+                    break;
+                }
             }
         }
-        fat_array[new_free_block] = FAT_EOC;
+        if (fds[fd].open_file->first_blk_index != FAT_EOC) {
+            fat_array[fds[fd].open_file->first_blk_index] = FAT_EOC;
+            cur = fds[fd].open_file->first_blk_index;
+        }
 
-        File_descriptors[fd].open_file->first_blk_index = new_free_block;
+        int bounce_blk_count = (fds[fd].offset + count - 1) / BLOCK_SIZE + 1;
+        for (int i = 1; i < bounce_blk_count; i++) {
+            if (fat_array[cur] == FAT_EOC) {
+                // nxt = first_free_block();
+                for (int j = 1; j < superblock->total_data_blks; j++) {
+                    if (fat_array[j] == 0) {
+                        fat_array[cur] = j;
+                        break;
+                    }
+                }
+            }
+            if (fat_array[cur] == FAT_EOC) {
+                break;
+            }
+            // fat_array[cur] = nxt;
+            fat_array[fat_array[cur]] = FAT_EOC;
+            cur = fat_array[cur];
+            fat_array[cur] = fat_array[fat_array[cur]];
+        }
     }
-    //new first block index
-    uint16_t head_index = File_descriptors[fd].open_file->first_blk_index + superblock->data_blk_idx;
-    //Write
-    //int block_write(size_t block, const void *buf);
-    int write_num = block_write(head_index, buf);
 
-    //need update size
+    /* read to bounce buffer */
+    int start_blk_idx = fds[fd].offset / BLOCK_SIZE;
+    int end_blk_idx = (fds[fd].offset + count - 1) / BLOCK_SIZE;
+    size_t total_buffer_blks = end_blk_idx - start_blk_idx + 1;
 
+    int total_fat_blks = 0;
+    uint16_t idx = fds[fd].open_file->first_blk_index;
+    while (idx != FAT_EOC) {
+        idx = fat_array[idx];
+        total_fat_blks++;
+    }
+    if (total_buffer_blks > total_fat_blks) {
+        total_buffer_blks = total_fat_blks;
+        end_blk_idx = total_buffer_blks + start_blk_idx - 1;
+    }
+    if (total_buffer_blks == 0) {
+        return 0;
+    }
+    uint8_t* buffer = NULL;
+    buffer = (uint8_t*)malloc(total_buffer_blks * BLOCK_SIZE * sizeof(uint8_t));
+    if (buffer == NULL) {
+        return -1;
+    }
+    idx =  fds[fd].open_file->first_blk_index;
+    for (int i = 0; i < end_blk_idx; i++) {
+        if (i >= start_blk_idx) {
+            block_read(superblock->data_blk_idx + idx, buffer + i * BLOCK_SIZE);
+        }
+        idx = fat_array[idx];
+    }
 
-    return write_num;
+    /* write to bounce buffer */
+    if (count > total_buffer_blks * BLOCK_SIZE - fds[fd].offset) {
+        count = total_buffer_blks * BLOCK_SIZE - fds[fd].offset;
+    }
+    memcpy(buffer + (fds[fd].offset % BLOCK_SIZE), buf, count);
+
+    /* write from bounce buffer to file system */
+    idx =  fds[fd].open_file->first_blk_index;
+    for (int i = 0; i < end_blk_idx; i++) {
+        if (i >= start_blk_idx) {
+           block_write(superblock->data_blk_idx + idx, buffer + i * BLOCK_SIZE);
+        }
+        idx = fat_array[idx];
+    }
+
+    if (fds[fd].open_file->filesize < fds[fd].offset + count) {
+        fds[fd].open_file->filesize = fds[fd].offset + count;
+    }
+    free(buffer);
+    return count;
 }
-
-/**
- * fs_read - Read from a file
- * @fd: File descriptor
- * @buf: Data buffer to be filled with data
- * @count: Number of bytes of data to be read
- *
- * Attempt to read @count bytes of data from the file referenced by file
- * descriptor @fd into buffer pointer by @buf. It is assumed that @buf is large
- * enough to hold at least @count bytes.
- *
- * The number of bytes read can be smaller than @count if there are less than
- * @count bytes until the end of the file (it can even be 0 if the file offset
- * is at the end of the file). The file offset of the file descriptor is
- * implicitly incremented by the number of bytes that were actually read.
- *
- * Return: -1 if file descriptor @fd is invalid (out of bounds or not currently
- * open). Otherwise return the number of bytes actually read.
- */
 
 int fs_read(int fd, void *buf, size_t count)
 {
-    // if (fd < 0 || fd >= FS_OPEN_MAX_COUNT) {
-    //     return -1;
-    // }
-    //
-    // if (File_descriptors[fd].open_file == NULL) {
-    //     return -1;
-    // }
-
-    // /* find length of bounce buffer */
-    // int start_idx = File_descriptors[fd].offset / BLOCK_SIZE;
-    // int end_idx = (offset + count - 1) / BLOCK_SIZE;
-
-    // uint16_t temp = File_descriptors[fd].open_file.first_blk_index;
-    // int last_idx = -1;
-    // while (temp != FAT_EOC) {
-    //     last_idx++;
-    //     temp = fat_array[temp];
-    // }
-    // if (end_idx > last_idx) {
-    //     end_idx = last_idx;
-    // }
-    // int length = end_idx - start_idx + 1;
-
-    // /* get bounce buffer */
-    // uint16_t bounce_buffer = (uint16_t*)malloc(BLOCK_SIZE / 2 * length * sizeof(uint16_t));
-
-    // temp = File_descriptors[fd].open_file.first_blk_index;
-    // for (int i = 0; i < )
-
-    // int first_block_idx = File_descriptors[fd].offset / BLOCK_SIZE;
-    // int last_block_idx  = (offset + count - 1) / BLOCK_SIZE;
-    //
-    // uint16_t temp = File_descriptors[fd].open_file.first_blk_index;
-    // int last_fat_blk_idx = -1;
-    // while (temp != FAT_EOC) {
-    //     last_fat_blk_idx++;
-    //     temp = fat_array[temp];
-    // }
-    //
-    // if (last_block_idx > last_fat_blk_idx) last_block_idx = last_fat_blk_idx;
-    
+    /* handle error */
     if (fd < 0 || fd >= FS_OPEN_MAX_COUNT) {
         return -1;
     }
-    
-    if (File_descriptors[fd].open_file == NULL) {
+    if (fds[fd].open_file == NULL) {
         return -1;
     }
 
+    /* read to bounce buffer */
+    int start_blk_idx = fds[fd].offset / BLOCK_SIZE;
+    int end_blk_idx = (fds[fd].offset + count - 1) / BLOCK_SIZE;
+    size_t total_buffer_blks = end_blk_idx - start_blk_idx + 1;
 
-    if (File_descriptors[fd].open_file->filesize <= File_descriptors[fd].offset) {
-    	return 0;
+    int total_fat_blks = 0;
+    uint16_t idx = fds[fd].open_file->first_blk_index;
+    while (idx != FAT_EOC) {
+        idx = fat_array[idx];
+        total_fat_blks++;
+    }
+    if (total_buffer_blks > total_fat_blks) {
+        total_buffer_blks = total_fat_blks;
+        end_blk_idx = total_buffer_blks + start_blk_idx - 1;
+    }
+    if (total_buffer_blks == 0) {
+        return 0;
+    }
+    uint8_t* buffer = NULL;
+    buffer = (uint8_t*)malloc(total_buffer_blks * BLOCK_SIZE * sizeof(uint8_t));
+    if (buffer == NULL) {
+        return -1;
+    }
+    idx =  fds[fd].open_file->first_blk_index;
+    for (int i = 0; i < end_blk_idx; i++) {
+        if (i >= start_blk_idx) {
+            block_read(superblock->data_blk_idx + idx, buffer + i * BLOCK_SIZE);
+        }
+        idx = fat_array[idx];
     }
 
-    //offset reach eof
-    if (File_descriptors[fd].open_file->first_blk_index == FAT_EOC) {
-    	return 0;
+    /* read from bounce buffer to buf */
+    if (count > fds[fd].open_file->filesize - fds[fd].offset) {
+        count = fds[fd].open_file->filesize - fds[fd].offset;
     }
-
-
-    int start_idx = File_descriptors[fd].offset / BLOCK_SIZE;
-    int end_idx = (File_descriptors[fd].offset + count - 1) / BLOCK_SIZE;
-
-    uint16_t temp = File_descriptors[fd].open_file->first_blk_index;
-    int last_idx = -1;
-    while (temp != FAT_EOC) {
-        last_idx++;
-        temp = fat_array[temp];
-    }
-    if (end_idx > last_idx) {
-        end_idx = last_idx;
-    }
-    int length = end_idx - start_idx + 1;
-    char* bounce_buffer = (char*)malloc(BLOCK_SIZE / 2 * length * sizeof(char));
-    for (int i = 0;i < superblock->total_data_blks;i++) {
-    	block_read(File_descriptors[fd].open_file->first_blk_index + superblock->data_blk_idx, bounce_buffer + (BLOCK_SIZE * i));
-    }
-
-    //whether count > offset
-    size_t offset_count;
-    if ((File_descriptors->open_file->filesize - File_descriptors->offset) < count) {
-    	offset_count = File_descriptors->open_file->filesize - File_descriptors->offset;
-    } else {
-    	offset_count = count;
-    }
-
-    //copy data from bounce_buffer
-    size_t file_offset = File_descriptors->offset % BLOCK_SIZE;
-    memcpy(buf, (file_offset + bounce_buffer), offset_count);
-    return offset_count;
+    memcpy(buf, buffer + (fds[fd].offset % BLOCK_SIZE), count);
+    fds[fd].offset += count;
+    free(buffer);
+    return count;
 }
